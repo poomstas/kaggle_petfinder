2683f6d7feaf51ada4bdfce887d0ddc8f9e89460
poomstas <b.lee@utexas.edu>
2021-11-21 00:15:29 +0900

    Added augmentations and specified target size when loading PetDataset

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-# import albumentations as A
+import albumentations as A
 from src.model import Xception #DenseNet121 # Add more as I go here
 from src.data import PetDataset
 from src.utils import print_config
@@ -52,14 +52,33 @@ elif config['model'] == 'DenseNet121':
     NORMAL_MEAN = [0.485, 0.456, 0.406]
     NORMAL_STD = [0.229, 0.224, 0.225]
 
+# %% Augmentations
+AUG_TRAIN = A.Compose([
+    A.HorizontalFlip(p=0.5),
+    A.VerticalFlip(p=0.5),
+    A.OneOf([A.RandomRotate90(p=0.5), A.Rotate(p=0.5)],p=0.5),
+    A.ColorJitter (brightness=0.2, contrast=0.2, p=0.3),
+    A.ChannelShuffle(p=0.3),
+    A.Normalize(NORMAL_MEAN, NORMAL_STD),
+    # A.RGBShift(r_shift_limit=20, g_shift_limit=20, b_shift_limit=20, always_apply=False, p=0.5)
+])
+
+AUG_VAL = A.Compose([
+    A.Normalize(NORMAL_MEAN, NORMAL_STD),
+])
+
 # %% No separate testing data used. "Test" is used as validation set.
-dataloader_train = DataLoader(dataset=PetDataset(type_trainval='train'),
+dataloader_train = DataLoader(dataset=PetDataset(type_trainval='train', 
+                                                 augments=AUG_TRAIN, 
+                                                 target_size=TARGET_SIZE),
                               batch_size = config['batch_size'],
                               drop_last = config['drop_last'],
                               shuffle = config['train_shuffle'],
                               num_workers = config['num_workers'])
 
-dataloader_val   = DataLoader(dataset=PetDataset(type_trainval='val'),
+dataloader_val   = DataLoader(dataset=PetDataset(type_trainval='val',
+                                                 augments=AUG_VAL, 
+                                                 target_size=TARGET_SIZE),
                               batch_size = config['batch_size'],
                               drop_last = config['drop_last'],
                               shuffle = config['val_shuffle'],
@@ -70,44 +89,12 @@ temp = iter(dataloader_train)
 img, metadata, pawpularity = next(temp)
 
 # %%
-DATA_DIR = './data/'
-TRAIN_DIR = os.path.join(DATA_DIR, 'train')
-TEST_DIR = os.path.join(DATA_DIR, 'test')
+# DATA_DIR = './data/'
+# TRAIN_DIR = os.path.join(DATA_DIR, 'train')
+# TEST_DIR = os.path.join(DATA_DIR, 'test')
 
-df_train = pd.read_csv('./data/train.csv')
-df_test  = pd.read_csv('./data/test.csv')
-df_train['filename'] = [Id + '.jpg' for Id in df_train['Id']]
-df_test['filename'] = [Id + '.jpg' for Id in df_test['Id']]
-
-# %%
-df_train
-# %%
-df_train.loc[0]['Info']
-
-# %%
-# df_train['metadata'] = df_train.loc[0].iloc[1:-2].tolist()
-df_train.loc[0].iloc[1:-2].values
-
-# pawpularity = df_train.loc[0].iloc[-2]
-
-# df_train['metadata'] = df_train.iloc
-
-
-
-df_train.loc[0]['Pawpularity']
-
-
-
-
-
-
-# %%
-
-# %%
-
-
-
-
-
-
+# df_train = pd.read_csv('./data/train.csv')
+# df_test  = pd.read_csv('./data/test.csv')
+# df_train['filename'] = [Id + '.jpg' for Id in df_train['Id']]
+# df_test['filename'] = [Id + '.jpg' for Id in df_test['Id']]
 
