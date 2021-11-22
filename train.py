765b0a96882062075186ca03c57b372f8b4049f0
poomstas <b.lee@utexas.edu>
2021-11-22 20:22:10 +0900

    Change training code to accept only one input (one image, for now).

--- train.py
+++ train.py
@@ -169,7 +169,8 @@ def train_model(model, dataloaders, criterion, optimizer, lr_scheduler, \
                 pawpularities = pawpularities.to(device)
 
                 with torch.set_grad_enabled(phase=='train'): # Enable grad only in train
-                    pawpulartiy_pred = model(images, metadata)
+                    pawpulartiy_pred = model(images) # Try with only one input for now
+                    # pawpulartiy_pred = model(images, metadata)
                     pawpulartiy_pred = torch.squeeze(pawpulartiy_pred) # See if this is necessary
 
                     loss = criterion(pawpulartiy_pred, pawpularities)
