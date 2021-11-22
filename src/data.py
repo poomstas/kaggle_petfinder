f5d86cfbba812a5d4c1f318753175d9c30071582
poomstas <b.lee@utexas.edu>
2021-11-22 18:17:59 +0900

    Cleaned up data.py

--- src/data.py
+++ src/data.py
@@ -20,9 +20,6 @@ class PetDataset(Dataset):
         self.testset                    = testset
         self.metadata_col_index_start   = self.df.columns.get_loc('Subject Focus')
         self.metadata_col_index_end     = self.df.columns.get_loc('Blur')
-
-        print(self.df)
-        print('='*90)
     
     def __len__(self):
         return len(self.df)
@@ -31,7 +28,7 @@ class PetDataset(Dataset):
         img_filename = self.df['Id'][index] + '.jpg'
         img_fullpath = os.path.join(self.img_folder, img_filename)
 
-        img = mpimg.imread(img_fullpath) # -> np.ndarray, RGB, uint8
+        img = mpimg.imread(img_fullpath)
 
         if (self.target_size, self.target_size) != img.shape[:2]:
             img = cv2.resize(img, (self.target_size, self.target_size))
@@ -41,24 +38,20 @@ class PetDataset(Dataset):
         
         start_indx = self.metadata_col_index_start
         end_indx   = self.metadata_col_index_end
-        metadata   = self.df.loc[index].iloc[start_indx:end_indx].values.astype(np.int) # -> np.array of ints
+        metadata   = self.df.loc[index].iloc[start_indx:end_indx].values.astype(np.int)
 
         if self.testset:
-            return img, metadata        # No pawpularity data
+            return img, metadata
         else:
             pawpularity = self.df.loc[index]['Pawpularity'].astype(np.float32) # -> np.float32
             return img, metadata, pawpularity
 
 # %%
 if __name__=='__main__':
+    ''' Test to see if the Dataset and DataLoader objects are working correctly. '''
     NORMAL_MEAN = [0.5, 0.5, 0.5]
     NORMAL_STD = [0.5, 0.5, 0.5]
     
-    # transforms = transforms.Compose([
-    #     transforms.Normalize(NORMAL_MEAN, NORMAL_STD),
-    #     transforms.ToTensor()
-    # ])
-
     transforms = A.Compose([
         A.HorizontalFlip(p=0.5),
         A.VerticalFlip(p=0.5),
@@ -69,7 +62,6 @@ if __name__=='__main__':
         A.ColorJitter (brightness=0.2, contrast=0.2, p=0.3),
         A.ChannelShuffle(p=0.3),
         A.Normalize(NORMAL_MEAN, NORMAL_STD),
-        # A.RGBShift(r_shift_limit=20, g_shift_limit=20, b_shift_limit=20, always_apply=False, p=0.5)
         ToTensorV2()
     ])
 
@@ -78,9 +70,6 @@ if __name__=='__main__':
                           shuffle=False,
                           batch_size=4,
                           num_workers=2)
-
-    # temp = iter(dataloader)
-    # temp.next()
 
     for images, metadata, pawpularities in dl_train:
         print(images.shape)
