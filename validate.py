24e21161b942a9f337821829e3eba34602b603bd
poomstas <b.lee@utexas.edu>
2021-11-26 14:25:54 +0900

    Improved scatterplot on testing (aspect ratio 1, target line, etc.)

--- validate.py
+++ validate.py
@@ -80,8 +80,31 @@ for batch_index, (images, metadata, pawpularities) in tqdm(enumerate(dataloader_
 pawpularities_pred_collect = np.squeeze(pawpularities_pred_collect)
 
 # %%
-plt.scatter(pawpularities_collect, pawpularities_pred_collect)
-plt.xlabel('Pawpularity'); plt.ylabel('Pawpularity Pred.')
+def adjustFigAspect(fig,aspect=1):
+    '''
+    Adjust the subplot parameters so that the figure has the correct
+    aspect ratio.
+    '''
+    xsize,ysize = fig.get_size_inches()
+    minsize = min(xsize,ysize)
+    xlim = .4*minsize/xsize
+    ylim = .4*minsize/ysize
+    if aspect < 1:
+        xlim *= aspect
+    else:
+        ylim /= aspect
+    fig.subplots_adjust(left=.5-xlim,
+                        right=.5+xlim,
+                        bottom=.5-ylim,
+                        top=.5+ylim)
+
+# %%
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(pawpularities_collect, pawpularities_pred_collect)
+ax.set_xlabel('Pawpularity'); ax.set_ylabel('Pawpularity Pred.')
+ax.plot(np.linspace(0,100,100), np.linspace(0,100,100), 'r--')
+adjustFigAspect(fig, aspect=1)
 plt.show()
 
 criterion = nn.MSELoss()
